218cbdff786d55b19c0c1d2d98cac2b772bff01e
mcagriaksoy <mcagriaksoy@ya.ru>
2023-09-05 22:52:30 +0200

    first gui adapted

--- main.py
+++ main.py
@@ -1,108 +1,48 @@
-import sys
-from block import State
+__author__ = 'mcagriaksoy'
 
-__author__ = 'alexisgallepe'
+from torrent_client import Run
+import sys, logging
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QListWidget
+from PyQt6.uic import loadUi
+from PyQt6.QtCore import QThreadPool, QRunnable, pyqtSlot
 
-import time
-import peers_manager
-import pieces_manager
-import torrent
-import tracker
-import logging
-import os
-import message
+torrent_name = ""
 
+class Worker(QRunnable):
+    @pyqtSlot()
+    def run(self):
+        logging.basicConfig(level=logging.ERROR)
+        run = Run(torrent_name)
+        run.start()
 
-class Run(object):
-    percentage_completed = -1
-    last_log_line = ""
-
+class App(QMainWindow):
     def __init__(self):
-        try:
-            torrent_file = sys.argv[1]
-        except IndexError:
-            logging.error("No torrent file provided!")
-            sys.exit(0)
-        self.torrent = torrent.Torrent().load_from_path(torrent_file)
-        self.tracker = tracker.Tracker(self.torrent)
+        QMainWindow.__init__(self)
+        loadUi('main.ui', self)
+        self.threadpool = QThreadPool()
+        self.pushButton.clicked.connect(self.load_torrent_file)
+        self.pushButton_2.clicked.connect(self.start_download)
+        self.pushButton_3.clicked.connect(self.stop_download)
 
-        self.pieces_manager = pieces_manager.PiecesManager(self.torrent)
-        self.peers_manager = peers_manager.PeersManager(self.torrent, self.pieces_manager)
+    def load_torrent_file(self):
+        # Open file dialog to select directory
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home', "Torrent Files (*.torrent)")
+        if fname[0]:
+            self.textBrowser.setText(fname[0])
+        
+        global torrent_name
+        torrent_name = fname[0]
+    
+    def start_download(self):
+        self.label_2.setText('Downloading..')
+        worker = Worker()
+        self.threadpool.globalInstance().start(worker)
 
-        self.peers_manager.start()
-        logging.info("PeersManager Started")
-        logging.info("PiecesManager Started")
-
-    def start(self):
-        peers_dict = self.tracker.get_peers_from_trackers()
-        self.peers_manager.add_peers(peers_dict.values())
-
-        while not self.pieces_manager.all_pieces_completed():
-            if not self.peers_manager.has_unchoked_peers():
-                time.sleep(1)
-                logging.info("No unchocked peers")
-                continue
-
-            for piece in self.pieces_manager.pieces:
-                index = piece.piece_index
-
-                if self.pieces_manager.pieces[index].is_full:
-                    continue
-
-                peer = self.peers_manager.get_random_peer_having_piece(index)
-                if not peer:
-                    continue
-
-                self.pieces_manager.pieces[index].update_block_status()
-
-                data = self.pieces_manager.pieces[index].get_empty_block()
-                if not data:
-                    continue
-
-                piece_index, block_offset, block_length = data
-                piece_data = message.Request(piece_index, block_offset, block_length).to_bytes()
-                peer.send_to_peer(piece_data)
-
-            self.display_progression()
-
-            time.sleep(0.1)
-
-        logging.info("File(s) downloaded successfully.")
-        self.display_progression()
-
-        self._exit_threads()
-
-    def display_progression(self):
-        new_progression = 0
-
-        for i in range(self.pieces_manager.number_of_pieces):
-            for j in range(self.pieces_manager.pieces[i].number_of_blocks):
-                if self.pieces_manager.pieces[i].blocks[j].state == State.FULL:
-                    new_progression += len(self.pieces_manager.pieces[i].blocks[j].data)
-
-        if new_progression == self.percentage_completed:
-            return
-
-        number_of_peers = self.peers_manager.unchoked_peers_count()
-        percentage_completed = float((float(new_progression) / self.torrent.total_length) * 100)
-
-        current_log_line = "Connected peers: {} - {}% completed | {}/{} pieces".format(number_of_peers,
-                                                                                         round(percentage_completed, 2),
-                                                                                         self.pieces_manager.complete_pieces,
-                                                                                         self.pieces_manager.number_of_pieces)
-        if current_log_line != self.last_log_line:
-            print(current_log_line)
-
-        self.last_log_line = current_log_line
-        self.percentage_completed = new_progression
-
-    def _exit_threads(self):
-        self.peers_manager.is_active = False
-        os._exit(0)
-
+    def stop_download(self):
+        self.label_2.setText('Stopped.')
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-
-    run = Run()
-    run.start()
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.exit(app.exec())
